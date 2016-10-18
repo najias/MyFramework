37b9f2c0cbaf6ae25eb71a5fc25ea26033caa450
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-10-17 20:10:11 -0400

    add Method in TestBase class

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -1,13 +1,21 @@
 package base;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
+
+import pages.MenuBar;
+import pages.SigninPage;
 
 @Listeners(listener.EliteListener.class)
 public class TestBase extends Driver{
 	
 	String  browserType = getProperty("browser");
 	String  appUrl = getProperty("appUrl");
+	
+	protected static SigninPage  signinPage;
+	protected static MenuBar  menuBar;
 	
 	@BeforeSuite
 	
@@ -18,6 +26,15 @@ public class TestBase extends Driver{
 		}catch(Exception  e){
 			e.printStackTrace();
 		}
+		
+		signinPage = PageFactory.initElements(driver,SigninPage.class);
+		menuBar = PageFactory.initElements(driver, MenuBar.class);
+		
+		
+	}
+	@AfterSuite
+	public void tearDown(){
+		driver.quit();
 	}
 
 }
