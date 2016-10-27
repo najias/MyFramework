3b715b974412f1bc9953c2ce8610580412122754
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-10-27 19:40:55 -0400

    add all WebElement for RegisterPage.

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -1,15 +1,48 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import base.PageBase;
 
 public class RegisterPage extends PageBase {
-	
+
 	@FindBy(name = "FirstName")
-	public  WebElement firstNameField;
-	
-	
+	public WebElement firstNameField;
+
+	@FindBy(name = "LastName")
+	public WebElement lastNameField;
+
+	@FindBy(name = "Gender")
+	public WebElement genderField;
+
+	@FindBy(name = "DateOfBirth")
+	public WebElement dateOfBirthField;
+
+	@FindBy(name = "PhoneNo")
+	public WebElement phoneNoField;
+
+	@FindBy(name = "UserType")
+	public WebElement userTypeField;
+
+	@FindBy(name = "Email")
+	public WebElement emailField;
+
+	@FindBy(name = "Password")
+	public WebElement passwordField;
+
+	@FindBy(name = "ConfirmPassword")
+	public WebElement confirmPasswordField;
+
+	@FindBy(name = "Captcha")
+	public WebElement captchaImage;
+
+	@FindBy(name = "submit")
+	public WebElement submitButton;
+
+	@FindBy(id = "error")
+	public List<WebElement> errorWebElementList;
 
 }
--- src/test/java/tests/TestRegisterPage.java
+++ src/test/java/tests/TestRegisterPage.java
@@ -1,7 +1,16 @@
 package tests;
 
+import org.testng.annotations.BeforeClass;
+
 import base.TestBase;
 
 public class TestRegisterPage extends TestBase{
+	
+	@BeforeClass
+	public void classSetUp(){
+		menuBar.registerPageLink.click();
+		
+		
+	}
 
 }
