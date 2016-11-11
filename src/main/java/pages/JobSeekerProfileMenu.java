4abad0c44a24f916e9c38aa5fab1bd16f304176a
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-11-11 16:04:50 -0500

    addEducationLink WebElement for JobSeekerProfileMenu

--- src/main/java/pages/JobSeekerProfileMenu.java
+++ src/main/java/pages/JobSeekerProfileMenu.java
@@ -1,7 +1,13 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import base.PageBase;
 
 public class JobSeekerProfileMenu extends PageBase{
+	
+	@FindBy(linkText = "ADD EDUCATION")
+	public WebElement addEducationLink;
 
 }
