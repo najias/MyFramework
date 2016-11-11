1c8c8c5070af4c91c55c2b4dfdbbddc3c40899f8
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-11-11 16:14:51 -0500

    add testDegreeTypeWithSelectOption

--- src/test/java/tests/TestAddNewEducation.java
+++ src/test/java/tests/TestAddNewEducation.java
@@ -1,6 +1,8 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import base.TestBase;
 
@@ -13,6 +15,14 @@ public class TestAddNewEducation extends TestBase {
 		menuBar.signInLink.click();
 		signinPage.signIn("nurul@yahoo.com", "365827");
 		jobSeekerProfileMenu.addEducationLink.click();
+	}
+	
+	@Test
+	public void testDegreeTypeWithSelectOption() {
+		addNewEducation.degreeType.sendKeys("High School Diploma/GED");
+		addNewEducation.submitButton.click();
+		Assert.assertEquals(addNewEducation.errorWebElementList.get(0).getText(), "*");
+
 	}
 
 }
