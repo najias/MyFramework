48bfd65a797ea0c9ed4b9fb9ec053ca52b170af4
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-11-11 16:13:12 -0500

    add BeforeClass setUp

--- src/test/java/tests/TestAddNewEducation.java
+++ src/test/java/tests/TestAddNewEducation.java
@@ -1,7 +1,18 @@
 package tests;
 
+import org.testng.annotations.BeforeClass;
+
 import base.TestBase;
 
 public class TestAddNewEducation extends TestBase {
+	
+
+	@BeforeClass
+	public void classSetUp() {
+
+		menuBar.signInLink.click();
+		signinPage.signIn("nurul@yahoo.com", "365827");
+		jobSeekerProfileMenu.addEducationLink.click();
+	}
 
 }
