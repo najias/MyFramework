1bb46563c24795a29af07cc7009ed2d286163fd3
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2017-03-09 18:37:02 -0500

    add test for TestAddNewExperience class

--- src/test/java/tests/TestAddNewExperience.java
+++ src/test/java/tests/TestAddNewExperience.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -24,6 +25,48 @@ public class TestAddNewExperience extends TestBase{
 		addNewExperience.jobTitleField.sendKeys("jhgfdgfsadtfyg");
 		addNewExperience.submitButton.click();
 		Assert.assertEquals(addNewExperience.errorWebElementList.get(0).getText(), "*");
+
+	}
+	@Test
+	public void testJobTitleWithEmpty() {
+		addNewExperience.submitButton.click();
+		Assert.assertEquals(addNewExperience.errorWebElementList.get(0).getText(), "* JobTitle can not be empty.");
+
+	}
+
+	@Test
+	public void testSelectYourCompanyWithSelectOption() {
+		addNewExperience.selectOptionFormDropdownList(addNewExperience.companyField);
+		addNewExperience.submitButton.click();
+
+		Assert.assertEquals(addNewExperience.errorWebElementList.get(1).getText(), "*");
+
+	}
+
+	@Test
+	public void testSelectYourCompanyWithEmpty() {
+		addNewExperience.submitButton.click();
+		Assert.assertEquals(addNewExperience.errorWebElementList.get(1).getText(), "* Company can not be empty.");
+	}
+
+	@Test
+	public void testJoiningDateWithValidDate() {
+		addNewExperience.joiningDateField.sendKeys(Keys.DELETE);
+		if (getProperty("browser").equalsIgnoreCase("chrome")) {
+			addNewExperience.joiningDateField.sendKeys("15/01/2016");
+		}else {
+		addNewExperience.joiningDateField.sendKeys("2016-01-15");
+		}
+		addNewExperience.submitButton.click();
+		Assert.assertEquals(addNewExperience.errorWebElementList.get(2).getText(), "*");
+
+	}
+
+	@Test
+	public void testJoiningDateWithEmpty() {
+		//addNewExperience.joiningDateField.clear();
+		addNewExperience.submitButton.click();
+		Assert.assertEquals(addNewExperience.errorWebElementList.get(2).getText(), "* JoiningDate can not be empty.");
 
 	}
 
