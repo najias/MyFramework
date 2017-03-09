3e3cbcf2b58fbd29deb0387235f5a46405373a34
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2017-03-09 17:55:34 -0500

    add BeforeClass setup for TestAddNewExperience class

--- src/test/java/tests/TestAddNewExperience.java
+++ src/test/java/tests/TestAddNewExperience.java
@@ -1,7 +1,15 @@
 package tests;
 
+import org.testng.annotations.BeforeClass;
+
 import base.TestBase;
 
 public class TestAddNewExperience extends TestBase{
+	
+	@BeforeClass
+	public void classSetUp() {
+		menuBar.signInLink.click();
+		signinPage.signIn("nurul@yahoo.com", "365827");
+	}
 
 }
