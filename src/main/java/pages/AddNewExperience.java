ce5a5973b61b8cfd36d56dd3125e33d77f77ff88
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2017-03-09 17:49:04 -0500

    add jobTitle WebElement for addNewExperience

--- src/main/java/pages/AddNewExperience.java
+++ src/main/java/pages/AddNewExperience.java
@@ -1,7 +1,13 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import base.PageBase;
 
 public class AddNewExperience extends PageBase{
+	
+	@FindBy(name = "JobTitle")
+	public WebElement jobTitleField;
 
 }
